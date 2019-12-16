1c65b101a4da70c4c788e79de6ee936bb561af68
Ihor Sofiychuk <i.sophiychuk@viseven.com>
2019-12-16 10:44:15 +0200

    update(*): add layout

---
+++ js/multiple_items.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.multiple_items').slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: true,
+      dots: false,
+      slidesToShow: 5,
+      variableWidth: true,
+    });
+  });
